65572af50436466e0fd40ecff0c0c6dcf407ec22
Apple_Lzzy09 <apple_lzzy09@Apple-Lzzy09deMac-mini.local>
2017-11-07 12:12:10 +0800

    上拉刷新和下拉完善

---
+++ Fx_WeiBo/Fx_WeiBo/class/Home/view/FXLoadFooter.h
@@ -0,0 +1,13 @@
+//
+//  FXLoadFooter.h
+//  Fx_WeiBo
+//
+//  Created by Apple_Lzzy09 on 2017/11/7.
+//  Copyright © 2017年 FX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FXLoadFooter : UIView
+
+@end
---
+++ Fx_WeiBo/Fx_WeiBo/class/Home/view/FXLoadFooter.m
@@ -0,0 +1,21 @@
+//
+//  FXLoadFooter.m
+//  Fx_WeiBo
+//
+//  Created by Apple_Lzzy09 on 2017/11/7.
+//  Copyright © 2017年 FX. All rights reserved.
+//
+
+#import "FXLoadFooter.h"
+
+@implementation FXLoadFooter
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
